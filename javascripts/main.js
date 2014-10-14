d807b13bb9e4df8653f9c5eb796bf7d30dd49ae7
DH3ALEJANDRO <dhe.my.x100pre@gmail.com>
2014-10-14 10:52:06 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
+console.time("Array initialize");
+
 console.log('This would be the main JS file.');
+
+
+function doAjax(url, callback) {
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            callback(xmlhttp.responseText);
+        }
+    }
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+};
+
+doAjax("/data/", function(responseText){
+    console.log(responseText);
+});
+
+console.timeEnd("Array initialize");
