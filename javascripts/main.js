97cc333f78c6b30b6dab6113f2b79c19405f87bf
DH3ALEJANDRO <dhe.my.x100pre@gmail.com>
2014-10-14 19:42:32 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,25 +3,29 @@ console.time("Array initialize");
 console.log('This would be the main JS file.');
 
 
-function doAjax(url, callback) {
-    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-
+function data(url, callback){
+var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
     xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            callback(xmlhttp.responseText);
+        //if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                //document.body.className = 'ok';
+                callback(xmlhttp.responseText);
+            } else {
+                //document.body.className = 'error';
+            }
         }
     }
-
     xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-};
+    xmlhttp.send( null );
+}
 
-doAjax("/javascripts/data.json", function(responseText){
-    console.log(responseText);
+data("/javascripts/data.json", function(responseText){
+    document.body.innerHTML+=responseText;
 });
 
-doAjax("/json.json", function(responseText){
-    console.log(responseText);
+data("/json.json", function(responseText){
+    document.body.innerHTML+=responseText;
 });
 
 console.timeEnd("Array initialize");
